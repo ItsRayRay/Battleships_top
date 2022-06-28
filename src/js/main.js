06ad62191917de07cb41e61dfa43d9f372c0d59a
ItsRayRay <r.boerema21@gmail.com>
2022-06-29 01:05:57 +0200

    fix menustreen

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+import func from './func';
+import typeWriter from './typewriter';
+
+
+
+
+func();
+typeWriter();
---
+++ src/js/typewriter.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt = 'Lets Play Battleship! What`s your name?'; /* The text */
+var speed = 30; /* The speed/duration of the effect in milliseconds */
+
+export default function typeWriter() {
+
+
+
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+
+    // if text is typing play sound
+    if (txt.charAt(i) === ' ' || txt.charAt(i) === '!' || txt.charAt(i) === '.' || txt.charAt(i) === '?')  { 
+      var audio = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+      audio.play();
+    }
+  }
+}
