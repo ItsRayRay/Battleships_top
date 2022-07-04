81194502783b1df04119dfa1bcebf6a79da25a4a
ItsRayRay <r.boerema21@gmail.com>
2022-07-04 12:21:10 +0200

    add interactive clicking to seamap

--- src/js/Selectplacement.js
+++ src/js/Selectplacement.js
@@ -1,3 +1,5 @@
+import player from "./player.js";
+
 
 export default function Selectplacement(map) {
 
@@ -5,13 +7,24 @@ export default function Selectplacement(map) {
     const choosePlacementContainer = document.createElement("div");
     choosePlacementContainer.classList.add("nes-container", "is-rounded", "is-dark");
     choosePlacementContainer.id = "choosePlacement";
+
+   const selectBoatWrapper = document.createElement("div");
+    selectBoatWrapper.classList.add("nes-container", "is-rounded", "is-dark");
+    selectBoatWrapper.id = "selectBoatWrapper";
+
     const choosePlacementInner = document.createElement("div");
     choosePlacementInner.classList.add("nes-inner");
     const choosePlacementTitle = document.createElement("p");
-    choosePlacementTitle.innerHTML = "Drop your Ships";
+    choosePlacementTitle.innerHTML = "Drop your Ships ";
     const choosePlacementInput = document.createElement("div");
     choosePlacementInput.classList.add("nes-input");
     const startButton = document.createElement("a");
+    const rotateButton = document.createElement("a");
+    rotateButton.innerHTML = "Rotate";
+    rotateButton.classList.add("nes-btn", "is-primary");
+    rotateButton.id = "rotateButton";
+    rotateButton.href = "#";
+
     startButton.classList.add("nes-btn");
     startButton.href = "#";
     startButton.innerHTML = "Start Game";
@@ -22,8 +35,7 @@ export default function Selectplacement(map) {
     choosePlacementInner.appendChild(choosePlacementTitle);
     choosePlacementInner.appendChild(choosePlacementInput);
     choosePlacementInput.appendChild(startButton);
-
-
+    choosePlacementInput.appendChild(rotateButton);
 
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ import player from "./player.js";
 import typeWriter from "./typewriter.js";
 import Selectplacement from "./Selectplacement.js";
 
-typeWriter();
 
 
 const carrier = new ship("Carrier", 5, [], false);
@@ -21,15 +20,25 @@ const mapPlayerTwo = new Map(10);
 const playerOne = new player("Player One", mapPlayerOne, allShips, true);
 const playerTwo = new player("Player Two", mapPlayerTwo, allShips, false);
 
+playerTwo.attack(mapPlayerOne, 0, 0);
+playerOne.attack(mapPlayerTwo, 0, 0);
 
-Selectplacement();
+console.log(mapPlayerOne.map);
+console.log(mapPlayerTwo.map);
+
 
 
 const seaMap = document.createElement("div")
 
 
+typeWriter();
 
-mapPlayerOne.map.forEach(row => {
+Selectplacement();
+
+
+function placeShips (dataPlayer) {
+
+    mapPlayerOne.map.forEach(row => {
     row.forEach(cell => {
 
         console.log(cell);
@@ -39,11 +48,69 @@ mapPlayerOne.map.forEach(row => {
         seaMap.classList.add("seaMap");
         document.querySelector("#choosePlacement").appendChild(seaMap);
         seaMap.appendChild(cell);
-
+        
     }   
     )
 
+})
+"🟧"
+ //add an event listener for each cell that listens for a click and changes color with hover
+seaMap.addEventListener("click", function(e) {
+    console.log(e.target);
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML === "🟧") {
+        e.target.innerHTML = "⬛";
+    }
+}
+)
 
-
+seaMap.addEventListener("mouseover", function(e) {
+    console.log(e.target);
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML === "🟦") {
+        e.target.innerHTML = "🟧";
+    }
 })
 
+seaMap.addEventListener("mouseout", function(e) {
+    console.log(e.target);
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML === "🟧") {
+        e.target.innerHTML = "🟦";
+    }
+})
+
+    // if the cell is clicked, then the ship will be placed
+    // if the cell is clicked again, then the ship will be removed
+    // if the cell is clicked again, then the ship will be placed
+    // add a id to the cell that will be the position of the ship
+    // push the position to the map array that will be used in the game
+    // if the ship is placed, then the cell will be changed to the symbol of the ship
+
+    console.log(dataPlayer)
+
+    console.log(dataPlayer.ships[0].size)
+    
+    const allShipSizes = dataPlayer.ships.map(ship => ship.size);
+
+    console.log(allShipSizes)
+
+    const shipSize5Carrier = allShipSizes[0];
+    const shipSize4BattleShip = allShipSizes[1];
+    const shipSize3Cruizer = allShipSizes[2];
+    const shipSize3SubMarine = allShipSizes[3];
+    const shipSize2Destroyer = allShipSizes[4];
+
+    console.log(shipSize5Carrier)
+    console.log(shipSize4BattleShip)
+    console.log(shipSize3Cruizer)
+    console.log(shipSize3SubMarine)
+    console.log(shipSize2Destroyer)
+
+
+
+
+    
+}
+
+placeShips(playerOne)
