9fa0ddc2a2716486923a3ff09e7389e69b5b40d3
ItsRayRay <r.boerema21@gmail.com>
2022-07-04 14:49:11 +0200

    add inique id to each cell on map for reference

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
-import _ from "lodash";
+import _, { indexOf } from "lodash";
 import ship from "./ship.js";
 import Map from "./map.js";
 import player from "./player.js";
 import typeWriter from "./typewriter.js";
 import Selectplacement from "./Selectplacement.js";
-
-
 
 const carrier = new ship("Carrier", 5, [], false);
 const battleship = new ship("Battleship", 4, [], false);
@@ -20,97 +18,79 @@ const mapPlayerTwo = new Map(10);
 const playerOne = new player("Player One", mapPlayerOne, allShips, true);
 const playerTwo = new player("Player Two", mapPlayerTwo, allShips, false);
 
-playerTwo.attack(mapPlayerOne, 0, 0);
+playerOne.attack(mapPlayerTwo, 2, 2);
 playerOne.attack(mapPlayerTwo, 0, 0);
-
-console.log(mapPlayerOne.map);
-console.log(mapPlayerTwo.map);
-
+playerOne.attack(mapPlayerTwo, 0, 0);
+playerOne.attack(mapPlayerTwo, 1, 0);
 
 
-const seaMap = document.createElement("div")
 
+const seaMap = document.createElement("div");
 
 typeWriter();
 
 Selectplacement();
 
+function placeShips(dataPlayer) {
 
-function placeShips (dataPlayer) {
 
-    mapPlayerOne.map.forEach(row => {
-    row.forEach(cell => {
 
-        console.log(cell);
-        cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.innerHTML = "🟦";
-        seaMap.classList.add("seaMap");
-        document.querySelector("#choosePlacement").appendChild(seaMap);
-        seaMap.appendChild(cell);
-        
-    }   
-    )
 
-})
-"🟧"
- //add an event listener for each cell that listens for a click and changes color with hover
-seaMap.addEventListener("click", function(e) {
-    console.log(e.target);
-    console.log(e.target.innerHTML);
-    if (e.target.innerHTML === "🟧") {
-        e.target.innerHTML = "⬛";
+const playerMap = mapPlayerOne.map;
+
+for (let i = 0; i < playerMap.length; i++) {
+  for (let j = 0; j < playerMap[i].length; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = `${i},${j}`;
+    cell.innerHTML = "🟦";
+    seaMap.classList.add("seaMap");
+    document.querySelector("#choosePlacement").appendChild(seaMap);
+    seaMap.appendChild(cell);
     }
-}
-)
+    }
 
-seaMap.addEventListener("mouseover", function(e) {
-    console.log(e.target);
-    console.log(e.target.innerHTML);
+
+
+
+  //add an event listener for each cell that listens for a click and changes color with hover
+  seaMap.addEventListener("click", function (e) {
+    if (e.target.innerHTML === "🟧") {
+      e.target.innerHTML = "⬛";
+    }
+  });
+
+  seaMap.addEventListener("mouseover", function (e) {
     if (e.target.innerHTML === "🟦") {
-        e.target.innerHTML = "🟧";
+      e.target.innerHTML = "🟧";
     }
-})
+  });
 
-seaMap.addEventListener("mouseout", function(e) {
-    console.log(e.target);
-    console.log(e.target.innerHTML);
+  seaMap.addEventListener("mouseout", function (e) {
     if (e.target.innerHTML === "🟧") {
-        e.target.innerHTML = "🟦";
+      e.target.innerHTML = "🟦";
     }
-})
+  });
 
-    // if the cell is clicked, then the ship will be placed
-    // if the cell is clicked again, then the ship will be removed
-    // if the cell is clicked again, then the ship will be placed
-    // add a id to the cell that will be the position of the ship
-    // push the position to the map array that will be used in the game
-    // if the ship is placed, then the cell will be changed to the symbol of the ship
-
-    console.log(dataPlayer)
-
-    console.log(dataPlayer.ships[0].size)
-    
-    const allShipSizes = dataPlayer.ships.map(ship => ship.size);
-
-    console.log(allShipSizes)
-
-    const shipSize5Carrier = allShipSizes[0];
-    const shipSize4BattleShip = allShipSizes[1];
-    const shipSize3Cruizer = allShipSizes[2];
-    const shipSize3SubMarine = allShipSizes[3];
-    const shipSize2Destroyer = allShipSizes[4];
-
-    console.log(shipSize5Carrier)
-    console.log(shipSize4BattleShip)
-    console.log(shipSize3Cruizer)
-    console.log(shipSize3SubMarine)
-    console.log(shipSize2Destroyer)
+  // if the cell is clicked, then the ship will be placed
+  // if the cell is clicked again, then the ship will be removed
+  // if the cell is clicked again, then the ship will be placed
+  // add a id to the cell that will be the position of the ship
+  // push the position to the map array that will be used in the game
+  // if the ship is placed, then the cell will be changed to the symbol of the ship
 
 
+  const allShipSizes = dataPlayer.ships.map((ship) => ship.size);
+
+ 
+
+  const shipSize5Carrier = allShipSizes[0];
+  const shipSize4BattleShip = allShipSizes[1];
+  const shipSize3Cruizer = allShipSizes[2];
+  const shipSize3SubMarine = allShipSizes[3];
+  const shipSize2Destroyer = allShipSizes[4];
 
 
-    
 }
 
-placeShips(playerOne)
+placeShips(playerOne);
--- src/js/map.js
+++ src/js/map.js
@@ -1,13 +1,13 @@
 export default class Map {
     constructor(size) {
-        this.size = size;
-        this.map = [];
-        for (let i = 0; i < size; i++) {
-            this.map[i] = [];
-            for (let j = 0; j < size; j++) {
-                this.map[i][j] = "🟦";
+            this.size = size;
+            this.map = [];
+            for (let i = 0; i < size; i++) {
+                this.map[i] = [];
+                for (let j = 0; j < size; j++) {
+                    this.map[i][j] = "🟦";
+                }
             }
-        }
     }
 }
 
